9c3368195549bd4c025ea5157d1816edf26e3d4d
Dallin Fahnestock <dfahn03@gmail.com>
2019-07-23 12:32:23 -0600

    refactoring bootstrap flow

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,31 +1,25 @@
 <template>
-  <div class="home">
-    <div class="row d-flex">
-      <div class="col">
-        <nav class="navbar navbar-light d-flex">
-          <a class="navbar-brand" href="#">
-            <img src="../assets/K-2.jpg" width="60" height="60" class="d-inline-block align-top" alt="">
-            <h4 class="float-right mt-2 ml-1 site-title">KeepSake</h4>
-          </a>
-          <h1 class="home-title mt-2">Welcome {{user.username}}</h1>
-          <button v-if="user.id" @click="logout" class="btn btn-sm btn-danger">Logout</button>
-          <router-link v-else :to="{name: 'Login'}"><button type="button" class="btn btn-sm btn-success">Login</button>
-          </router-link>
-        </nav>
-      </div>
+  <div class="row home">
+    <div class="col-12 p-0">
+      <nav class="navbar navbar-dark p-1">
+        <a class="navbar-brand" href="#">
+          <img src="../assets/K-2.jpg" width="60" height="60" class="d-inline-block align-top" alt="">
+          <h4 class="float-right mt-2 ml-1 site-title">KeepSake</h4>
+        </a>
+        <h1 class="home-title mt-2">Welcome {{user.username}}</h1>
+        <button v-if="user.id" @click="logout" class="btn btn-sm btn-danger">Logout</button>
+        <router-link v-else :to="{name: 'Login'}"><button type="button" class="btn btn-sm btn-success">Login</button>
+        </router-link>
+      </nav>
     </div>
-    <div class="row">
-      <div class="col">
-        <!-- <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div> -->
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text"> </p>
+        </div>
       </div>
     </div>
   </div>
@@ -48,13 +42,29 @@
 </script>
 
 <style>
+  .home {
+    /* height: 100%;
+    width: 100%; */
+    min-width: 100vw;
+    min-height: 100vh;
+    margin: 0px 0px;
+    padding: 0px 0px;
+    background-image: url("../assets/home-bg-2.jpg");
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: 100% 100%;
+    background-position: center;
+  }
+
   .site-title {
     font-family: 'Lobster', cursive;
     font-size: 2rem;
   }
 
   .home-title {
-    /* font-family: 'Yanone Kaffeesatz', sans-serif; */
     font-family: 'Acme', sans-serif;
+    color: white;
+    /* font-family: 'Yanone Kaffeesatz', sans-serif; */
   }
 </style>
