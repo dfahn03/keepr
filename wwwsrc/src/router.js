a3d21454730053d70300b5a3f65cfd3ae673ef9a
Dallin Fahnestock <dfahn03@gmail.com>
2019-09-05 22:43:40 -0600

    clean-up to redeploy

--- wwwsrc/src/router.js
+++ wwwsrc/src/router.js
@@ -15,13 +15,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     },
     {
       path: '/dashboard',
